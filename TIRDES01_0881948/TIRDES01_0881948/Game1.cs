0926ba17563b5f0a345127ad78809963ae43f0e8
rikvd <rikvandoorn@gmail.com>
2015-09-22 08:04:46 -0700

    Added multiple asteroids + plasma Added a switch statement for code that takes longer than 1 frame.

--- TIRDES01_0881948/TIRDES01_0881948/Game1.cs
+++ TIRDES01_0881948/TIRDES01_0881948/Game1.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Linq;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 using System;
 
 namespace TIRDES01_0881948
@@ -16,109 +18,176 @@ namespace TIRDES01_0881948
             Content.RootDirectory = "Content";
         }
 
+        int gameLogicScriptPC = 0;
+        int rndNumberLine1, iLine1, rndNumberLine5, iLine5;
+        float timeToWaitLine3, timeToWaitLine4, timeToWaitLine7, timeToWaitLine8;
+
+
+        Random randomGenerator = new Random();
+        Texture2D shipAppearance, asteroidAppearance, plasmaAppearance;
+        List<Vector2> asteroidPositions = new List<Vector2>();
+        List<Vector2> plasmaPositions = new List<Vector2>();
         Vector2 shipPosition;
-        Texture2D shipAppearance;
-        Vector2 astPosition;
-        Texture2D astAppearance;
-        Vector2 plasmaPosition;
-        Texture2D plasmaAppearance;
-
-        int shipSpeed = 4;
-        bool astAlive = true;
-        bool plasmaAlive = false;
-        protected override void Initialize()
+        float shipSpeed;
+        protected override void LoadContent()
         {
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            shipPosition = new Vector2(300.0f, 400.0f);
+
             shipAppearance = Content.Load<Texture2D>("ship.png");
-
-            astPosition = new Vector2(300.0f, 50.0f);
-            
-            astAppearance = Content.Load<Texture2D>("asteroid.png");
-
+            asteroidAppearance = Content.Load<Texture2D>("asteroid.png");
             plasmaAppearance = Content.Load<Texture2D>("plasmaSmall.png");
-
-            base.Initialize();
-
+            shipPosition = new Vector2(300.0f, 400.0f);
+            shipSpeed = 100.0f;
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-            {
+            KeyboardState ks = Keyboard.GetState();
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (ks.IsKeyDown(Keys.Escape)) {
                 Exit();
             }
 
+            var newPlasmaPositions =
+                (from plasmaPosition in plasmaPositions
+                 let colliders =
+                     from asteroidPosition in asteroidPositions
+                     where Vector2.Distance(plasmaPosition, asteroidPosition) < 20.0f
+                     select asteroidPosition
+                 where plasmaPosition.X > 50.0f &&
+                       plasmaPosition.X < 600.0f &&
+                       plasmaPosition.Y > 50.0f &&
+                       plasmaPosition.Y < 600.0f &&
+                       colliders.Count() == 0
+                 select plasmaPosition - Vector2.UnitY * 200.0f * deltaTime).ToList();
+            if (ks.IsKeyDown(Keys.Space))
+                newPlasmaPositions.Add(shipPosition);
+
+            var newAsteroidPositions =
+                (from asteroidPosition in asteroidPositions
+                 let colliders =
+                     from plasmaPosition in plasmaPositions
+                     where Vector2.Distance(plasmaPosition, asteroidPosition) < 20.0f
+                     select asteroidPosition
+                 where asteroidPosition.X > 50.0f &&
+                       asteroidPosition.X < 600.0f &&
+                       asteroidPosition.Y > 50.0f &&
+                       asteroidPosition.Y < 300.0f &&
+                       colliders.Count() == 0
+                 select asteroidPosition + Vector2.UnitY * 100.0f * deltaTime).ToList();
+
             Vector2 shipMovement = new Vector2(0.0f, 0.0f);
-            if (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                shipMovement += new Vector2(-1.0f, 0.0f) * shipSpeed;
+            if (ks.IsKeyDown(Keys.A) || ks.IsKeyDown(Keys.Left)) {
+                shipMovement += new Vector2(-1.0f, 0.0f);
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                shipMovement += new Vector2(1.0f, 0.0f) * shipSpeed;
+            if (ks.IsKeyDown(Keys.D) || ks.IsKeyDown(Keys.Right)) {
+                shipMovement += new Vector2(1.0f, 0.0f);
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Space)&&(plasmaAlive == false))
-            {
+            Vector2 shipVelocity = shipMovement * shipSpeed;
+            shipPosition += shipVelocity * deltaTime;
 
-                plasmaPosition = new Vector2((shipPosition.X+(shipAppearance.Width/2)-(plasmaAppearance.Width/2)), shipPosition.Y);
-                plasmaAlive = true;
-            }
-            if (plasmaPosition.Y < (astPosition.Y + astAppearance.Height) && (plasmaPosition.X < astPosition.X + astAppearance.Width) && plasmaPosition.X + (plasmaAppearance.Width/2) > astPosition.X)
-            {
-                plasmaAlive = false;
-                astAlive = false;
-            }
-            shipPosition += shipMovement;
-            plasmaPosition += new Vector2(0.0f, -5.0f);
-            if ((shipPosition.X < (astPosition.X + 20)) && (shipPosition.X > (astPosition.X - 50)))
-            {
-                if ((shipPosition.Y > (astPosition.Y - 20)) && (shipPosition.Y < (astPosition.Y + 50)))
-                {
-                    astAlive = false;
-                }
-            }
-            if (plasmaPosition.Y <= 0)
-            {
-                plasmaAlive = false;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.C))
-            {
-                astAlive = true;
-            }
-            if (!astAlive)
-            {
-                Random rnd = new Random();
-                int posX = rnd.Next(1, Window.ClientBounds.Width);
-                float pos = Convert.ToSingle(posX);
-                astPosition = new Vector2(pos, 50.0f);
-                astAlive = true;
+
+            switch (gameLogicScriptPC) {
+                case 0:
+                    if (true) {
+                        gameLogicScriptPC = 1;
+                        iLine1 = 1;
+                        rndNumberLine1 = randomGenerator.Next(20, 60);
+                    }
+                    else {
+                        gameLogicScriptPC = 9;
+                    }
+                    break;
+                case 1:
+                    if (iLine1 <= rndNumberLine1) {
+                        gameLogicScriptPC = 2;
+                    }
+                    else {
+                        gameLogicScriptPC = 4;
+                        timeToWaitLine4 = (float)(randomGenerator.NextDouble() * 2.0 + 5.0);
+                    }
+                    break;
+                case 2:
+                    newAsteroidPositions.Add(new Vector2((float)(randomGenerator.NextDouble() * 500.0 + 51.0), 51.0f));
+                    gameLogicScriptPC = 3;
+                    timeToWaitLine3 = (float)(randomGenerator.NextDouble() * 0.2 + 0.1);
+                    break;
+                case 3:
+                    timeToWaitLine3 -= deltaTime;
+                    if (timeToWaitLine3 > 0.0f) {
+                        gameLogicScriptPC = 3;
+                    }
+                    else {
+                        gameLogicScriptPC = 1;
+                        iLine1++;
+                    }
+                    break;
+                case 4:
+                    timeToWaitLine4 -= deltaTime;
+                    if (timeToWaitLine4 > 0.0f) {
+                        gameLogicScriptPC = 4;
+                    }
+                    else {
+                        gameLogicScriptPC = 5;
+                        iLine5 = 1;
+                        rndNumberLine5 = randomGenerator.Next(10, 20);
+                    }
+                    break;
+                case 5:
+                    if (iLine5 <= rndNumberLine5) {
+                        gameLogicScriptPC = 6;
+                    }
+                    else {
+                        gameLogicScriptPC = 8;
+                        timeToWaitLine8 = (float)(randomGenerator.NextDouble() * 2.0 + 5.0);
+                    }
+                    break;
+                case 6:
+                    newAsteroidPositions.Add(new Vector2((float)(randomGenerator.NextDouble() * 500.0 + 51.0), 51.0f));
+                    gameLogicScriptPC = 7;
+                    timeToWaitLine7 = (float)(randomGenerator.NextDouble() * 1.5 + 0.5);
+                    break;
+                case 7:
+                    timeToWaitLine7 -= deltaTime;
+                    if (timeToWaitLine7 > 0.0f) {
+                        gameLogicScriptPC = 7;
+                    }
+                    else {
+                        gameLogicScriptPC = 5;
+                        iLine5++;
+                    }
+                    break;
+                case 8:
+                    timeToWaitLine8 -= deltaTime;
+                    if (timeToWaitLine8 > 0.0f) {
+                        gameLogicScriptPC = 8;
+                    }
+                    else {
+                        gameLogicScriptPC = 0;
+                    }
+                    break;
+                default:
+                    break;
+
             }
 
-            if ((shipPosition.X + shipAppearance.Width < 0))
-            {
-                shipPosition.X = Convert.ToSingle(Window.ClientBounds.Width);
-            }
-            if (shipPosition.X > Convert.ToSingle(Window.ClientBounds.Width))
-            {
-                shipPosition.X = -shipAppearance.Width;
-            }
+            plasmaPositions = newPlasmaPositions;
+            asteroidPositions = newAsteroidPositions;
 
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin();
-            if (astAlive)
-            {
-                spriteBatch.Draw(astAppearance, astPosition, Color.Red);
-            }
             spriteBatch.Draw(shipAppearance, shipPosition, Color.White);
-            if (plasmaAlive)
-            {
-                spriteBatch.Draw(plasmaAppearance, plasmaPosition, Color.Black);
+            foreach (var plasmaPosition in plasmaPositions) {
+                spriteBatch.Draw(plasmaAppearance, plasmaPosition, Color.White);
+            }
+            foreach (var asteroidPosition in asteroidPositions) {
+                spriteBatch.Draw(asteroidAppearance, asteroidPosition, Color.White);
             }
             spriteBatch.End();
             base.Draw(gameTime);
